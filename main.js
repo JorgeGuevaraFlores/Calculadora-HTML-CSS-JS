922a06b9f95c6c985b7e769891f381ddcd3afa89
Jorge Guevara Flores <jorgegf720@gmail.com>
2022-02-26 15:25:02 -0600

    Funcion de suma automática hecha

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var punto = 1;
 var numero1 = null,
   numero2 = null,
   operador = null;
+let checkIgual=false;
 
 document.addEventListener("keydown", function (event) {
   // alert("Tecla presionada: "+event.keyCode)
@@ -29,35 +30,6 @@ const botonPulsado = (boton) => {
   }
 };
 
-// const teclaPulsada = (tecla) => {
-//   console.log(tecla.keyCode);
-//   if (tecla.keyCode == 110) {
-//     if (txtPantalla == 1 && punto == 1) {
-//       pantalla.value += "0.";
-//       punto = 0;
-//       txtPantalla = 0;
-//     } else if (punto == 1) {
-//       pantalla.value += tecla.key;
-//       punto = 0;
-//     }
-//   } else if (tecla.keyCode >= 48 && tecla.keyCode <= 57) {
-//     pantalla.value += tecla.key;
-//     txtPantalla = 0;
-//   } else if (tecla.keyCode >= 96 && tecla.keyCode <= 105) {
-//     pantalla.value += tecla.key;
-//     txtPantalla = 0;
-//   } else if (
-//     (tecla.keyCode >= 106 && tecla.keyCode <= 109) ||
-//     tecla.keyCode == 111
-//   ) {
-//     operacionTeclas(tecla.key);
-//   } else if (tecla.keyCode == 13) {
-//     igual();
-//   } else if (tecla.keyCode == 46) {
-//     borrar();
-//   }
-// };
-
 const operacion = (op) => {
   numero1 = pantalla.value;
   operador = op.innerText;
@@ -65,6 +37,7 @@ const operacion = (op) => {
   console.log(numero1 + " " + operador);
   punto = 1;
   txtPantalla = 1;
+  checkIgual = false;
 };
 
 const operacionTeclas = (op) =>{
@@ -75,15 +48,21 @@ const operacionTeclas = (op) =>{
   punto = 1;
   txtPantalla = 1;
 }
-
 const igual = () => {
-  numero2 = pantalla.value;
+  if(checkIgual){
+    numero1=pantalla.value;
+  } else {
+    numero2 = pantalla.value;
+  }
   let operacion = numero1 + " " + operador + " " + numero2;
   let resultado = eval(operacion);
   pantalla.value = resultado;
-  numero1 = null;
-  numero2 = null;
-  operador = null;
+  checkIgual=true;
+  console.log(numero1)
+  console.log(numero2)
+  // numero1 = null;
+  // numero2 = null;
+  // operador = null;
   console.log(operacion);
   console.log(resultado);
 };
